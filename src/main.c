e6caf8585c49a68c00623bbe4d78855e7c9fcc87
Anthony BARA <banthony@student.42.fr>
2017-08-25 00:18:48 +0200

    TestV2

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: banthony <banthony@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/10 15:19:51 by banthony          #+#    #+#             */
-/*   Updated: 2017/08/24 23:57:41 by banthony         ###   ########.fr       */
+/*   Updated: 2017/08/25 00:08:25 by banthony         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,17 @@ int main(void)
 	void *str;
 	size_t s = sizeof(t_area);
 
-	printf("Struct_Size:%lu\n", s);
+	printf("Struct t_area size:%lu\n", s);
+	printf("\tvoid\t*adr:%lu\n", sizeof(void*));
+	printf("\tsize_t\tsize:%lu\n", sizeof(size_t));
+	printf("\ts_list\t*flst:%lu\n", sizeof(t_list));
+	printf("\ts_area\t*next:%lu\n", sizeof(struct s_area));
+	printf("\tchar\ttag[8]:%lu\n", sizeof(char[8]));
+//	Demande d'une zone de 8
 	str = my_malloc(sizeof(char) * 8);
+//	Remplissage de la zone fournie
 	ft_strncpy(str, "DataData", 8);
+//	Affichage zone avec header
 	ft_print_memory(str - s, 64);
 }
 
--- src/malloc.c
+++ src/malloc.c
@@ -6,7 +6,7 @@
 /*   By: banthony <banthony@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/21 15:09:11 by banthony          #+#    #+#             */
-/*   Updated: 2017/08/24 23:54:32 by banthony         ###   ########.fr       */
+/*   Updated: 2017/08/25 00:18:16 by banthony         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,24 @@
 void	*my_malloc(size_t size)
 {
 	void *ptr;
-	t_area *test;
-	char end[8] = "THE END-";
+	t_area *tmp;
+	char end[8] = "THEEND->";
 
 	ptr = NULL;
 	if (!size)
 		return (NULL);
 	if (!(ptr = mmap(NULL, size + 1, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0)))
 		return (NULL);
-	ft_memset(ptr, '\0', size + 1);
-	if (!(test = (t_area*)mmap(NULL, sizeof(t_area), PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0)))
+	if (!(tmp = (t_area*)mmap(NULL, sizeof(t_area), PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0)))
 		return (NULL);
-	ft_memset(test, '\0', sizeof(t_area));
-	test->size = 42;
-	ft_strncpy(test->tag, end, 8);
-	test->adr = ptr;
-	return (test->adr);
+	ft_memset(ptr, '\0', size + 1);
+	tmp->size = 42;
+	ft_strncpy(tmp->tag, end, 8);
+	ptr = (void*)tmp;
+	return (&ptr[40]);
 }
+
+
 
 
 
